7e42ca7573fdfc68d4e49f7afedbbf009dc450d0
aljpetri <aljpetri@gmail.com>
2024-12-03 18:44:35 +0200

    Fixed identity formula and added verbose command line argument

--- Code_Alex/src/main.rs
+++ Code_Alex/src/main.rs
@@ -7,7 +7,6 @@ use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
 use bio::io::fastq;
 use edlib_rs::edlibrs::*;
-//use edlib::{align, AlignConfig, AlignmentResult};
 use itertools::Itertools;
 use rustc_hash::FxHashMap;
 use crate::side_functions::reverse_complement;
@@ -43,11 +42,11 @@ fn read_csv_to_map(filename: String) -> Vec<(String, String)> {
 fn write_outfile(filename: String, outfile_hashmap: FxHashMap<String,Vec<(String, usize, usize, i32)>>){
     let f = File::create(filename).expect("unable to create file");
     let mut buf_write = BufWriter::new(&f);
-    write!(buf_write ,"read_acc,fragment_id,start,stop,aln_length,matches,fragment_length\n").expect("We should be able to write the entries");
+    write!(buf_write ,"read_acc,fragment_id,start,stop,edit_distance,matches,fragment_length\n").expect("We should be able to write the entries");
     for entry in outfile_hashmap{
         let read_header=entry.0;
         for fragment in entry.1{
-            write!(buf_write ,"{},{},{},{}\n", read_header, fragment.0,fragment.1,fragment.2).expect("We should be able to write the entries");
+            write!(buf_write ,"{}, {}, {}, {}, {}\n", read_header, fragment.0, fragment.1, fragment.2, fragment.3).expect("We should be able to write the entries");
         }
     }
 
@@ -83,8 +82,11 @@ fn add_bw_frags (expected_fragments_vec: &mut Vec<(String,String)>){
 
 
 //recursive function that is used to detect the positions of the seperator in the read
-fn collect_sep_positions(sequence:&[u8], k:i32, expected_fragments_vec:&Vec<(String,String)>, previous_frags: usize,min_identity: f64) ->Vec<(i32,i32,i32,String,usize,usize)>{
-    println!("prev_frags {}",previous_frags);
+fn collect_sep_positions(sequence:&[u8], k:i32, expected_fragments_vec:&Vec<(String,String)>, previous_frags: usize,min_identity: f64, verbose: bool) ->Vec<(i32,i32,i32,String,usize,usize)>{
+    if verbose{
+        println!("prev_frags {}",previous_frags);
+    }
+
     let mut sep_positions= vec![];
     let mut config = EdlibAlignConfigRs::default();
     config.mode = EdlibAlignModeRs::EDLIB_MODE_HW;
@@ -92,27 +94,26 @@ fn collect_sep_positions(sequence:&[u8], k:i32, expected_fragments_vec:&Vec<(Str
     config.k = k;
     // best_alignments stores the best hits for each fragment, so we do not have to recompute every alignment
     let mut best_results = vec![];
-    let mut best_identity= 1f64;//set this to 1 for best yet
-    let mut best_len=sequence.len();
+    let mut best_identity= 0f64;//set this to 1 for best yet
+    let mut best_len= sequence.len();
     let mut best_frag: &str = "";
     let mut longest_frag= 0;
     let mut best_start= 0;
     let mut best_end= 0;
     let mut best_frag_str= best_frag.to_string();
-    let mut best_alignment:Vec<u8>=vec![];
+    let mut best_alignment:Vec<u8> = vec![];
     let mut best_dist= 1000;
     for fragment in expected_fragments_vec {
-
         let fragment_name = fragment.0.clone();
         //println!("Fragment: {}",fragment_name);
         let fragment_seq = fragment.1.as_bytes();
         let fragment_len = fragment_seq.len();
         let align_res = edlibAlignRs(fragment_seq, sequence, &config);
         if align_res.endLocations.is_some() {
-            let end_locs = align_res.endLocations.unwrap();
+            let end_locs = align_res.endLocations.clone().unwrap();
             //println!("Ends: {:?}",end_locs);
             //let mut start_locs=vec![];
-            let start_locs = align_res.startLocations.unwrap();
+            let start_locs = align_res.startLocations.clone().unwrap();
             for (loc_idx,end_loc) in end_locs.iter().enumerate() {
                 let alignment_start= *start_locs.get(loc_idx).unwrap();
                 let alignment_len:usize= (end_loc - alignment_start) as usize;
@@ -120,16 +121,18 @@ fn collect_sep_positions(sequence:&[u8], k:i32, expected_fragments_vec:&Vec<(Str
                 //println!("Alignment from{:?} to {:?} with ED {} : {:?}",start_locs,end_loc,align_res.editDistance,align_res.alignment);
                 //let this_identity = ((alignment_len-align_res.editDistance) - fragment_len as i32).abs();
                 //Worked best yet:
-                let this_identity= align_res.editDistance as f64 / fragment_len as f64;
+                //let this_identity= align_res.editDistance as f64 / fragment_len as f64;
+                let this_identity= (alignment_len as i32 - align_res.editDistance) as f64/ alignment_len as f64;
                 //let this_identity= fragment_len as f64/ alignment_len as f64;
-                if this_identity < min_identity && this_identity < best_identity && alignment_len < best_len{
+                //if this_identity < min_identity && this_identity < best_identity && alignment_len < best_len{
+                if this_identity > min_identity && this_identity >best_identity{
                     best_frag = &fragment_name;
                     best_frag_str = best_frag.to_string();
                     best_dist = align_res.editDistance;
                     best_identity = this_identity;
                     best_len = alignment_len;
                     best_start = alignment_start;
-                    //best_alignment = *align_res.getAlignment().unwrap();
+                    best_alignment = align_res.getAlignment().unwrap().clone();
                     //println!("Best identity {}",best_identity);
                     best_end = *end_loc as usize;
                     let best_alignment_frag = (best_frag_str.clone(), best_dist, alignment_start, best_end);
@@ -155,7 +158,7 @@ fn collect_sep_positions(sequence:&[u8], k:i32, expected_fragments_vec:&Vec<(Str
             }
         }
     }
-    if best_identity < 1f64{
+    if best_identity > 0f64{
         println!("best_fragment {}, start {}  end {} ED {}", best_frag_str, best_start, best_end, best_dist);
         println!("BFS: {}",best_frag_str);
         let best_total_pos= best_end + previous_frags;
@@ -177,7 +180,9 @@ struct Cli {
     #[arg(long, short, help="Path to input fastq file")]
     fastq: String,
     #[arg(long, short, help="Path to input fastq file")]
-    outfile: String
+    outfile: String,
+    #[arg(long,help="print additional information")]
+    verbose: bool
     /*#[arg(short,  help="Kmer length")]
     k: Option<usize>,
     #[arg(short, help=" window size")]
@@ -201,8 +206,7 @@ struct Cli {
     seeding: Option<String>,
     #[arg(long,help="quality threshold used for the data (standard: 0.9) ")]
     quality_threshold: Option<f64>,
-    #[arg(long,help="print additional information")]
-    verbose: bool,
+
     #[arg(long,help="Run the post clustering step during the analysis (small improvement for results but much higher runtime)")]
     post_cluster: bool,
     #[arg(long,help="Do not write the fastq_files (no write_fastq in isONclust1)")]
@@ -216,15 +220,21 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
     let min_identity= 0.75;
-    let outfilename=cli.outfile;// ="/home/alexanderpetri/Project3/SimulationResults/first4_fastqs/Alex/outfile";
+    let outfilename= cli.outfile;// ="/home/alexanderpetri/Project3/SimulationResults/first4_fastqs/Alex/outfile";
     let expected_fragments_filename= cli.expected;//"/home/alexanderpetri/Project3/Expected_fragments.csv";
+    let verbose=cli.verbose;
     let mut expected_fragments_vec: Vec<(String,String)> = vec![];
     expected_fragments_vec = read_csv_to_map(expected_fragments_filename);
     add_rev_comp_frags(&mut expected_fragments_vec);
-    add_bw_frags(&mut expected_fragments_vec);
-    println!("{:?}",expected_fragments_vec);
+    //add_bw_frags(&mut expected_fragments_vec);
+    if verbose{
+        println!("{:?}",expected_fragments_vec);
+    }
+
     expected_fragments_vec.sort_by(|a, b|b.1.len().partial_cmp(&a.1.len()).expect("REASON"));
-    println!("{:?}",expected_fragments_vec);
+    if verbose{
+        println!("{:?}",expected_fragments_vec);
+    }
     let long_fraglen= expected_fragments_vec.first().unwrap().1.len();
     let short_fraglen= expected_fragments_vec.last().unwrap().1.len();
     let filename=cli.fastq;// "/home/alexanderpetri/Project3/Fastqs/1_first_4.fastq";
@@ -232,34 +242,41 @@ fn main() {
     let k= k_inter.ceil() as i32;
     let mut frag_positions: Vec<(i32,i32,i32,String,usize,usize)>=vec![];
     let previous_frags: usize = 0;
-    println!("fragments range from {} to {} bp length",short_fraglen,long_fraglen);
+    if verbose{
+        println!("fragments range from {} to {} bp length", short_fraglen, long_fraglen);
+
+    }
     let mut reader = fastq::Reader::from_file(Path::new(&filename)).expect("We expect the file to exist");
     let mut config = EdlibAlignConfigRs::default();
     let mut range_end = 0;
     let mut aligned_seg;
-    let mut this_pos=vec![];
+    let mut this_pos= vec![];
     let mut outfile_hashmap = FxHashMap::default();
     config.mode = EdlibAlignModeRs::EDLIB_MODE_HW;
     config.k = 8;
+    //iterate over the reads in the fastq file
     for record in reader.records() {
-        let mut it_ct=0;
         let mut covering_vec = vec![];
         let mut prev_end = 0;
         let seq_rec = record.expect("invalid record");
         let header_new = seq_rec.id();
-        println!("{}", header_new);
+
         let mut sequence = seq_rec.seq();
-        println!("Sequence {:?}",std::str::from_utf8(sequence));
+
         let mut seq_cover_vec: Vec<Interval> = vec![];
-        println!("Readlength {}", sequence.len());
+        if verbose{
+            println!("{}", header_new);
+            println!("Readlength {}", sequence.len());
+            println!("Sequence {:?}",std::str::from_utf8(sequence));
+        }
+
         //part_frags_map maps the fragment position to the part it was found in as wee need to keep track of the parts.
         let mut part_frags_map: FxHashMap<u64,(String, Vec<(i32,i32,i32,String,usize,usize)>)> = FxHashMap::default();
         //get the initial fragment positions
-        frag_positions = collect_sep_positions(sequence, k, &expected_fragments_vec, previous_frags, min_identity).clone();
+        frag_positions = collect_sep_positions(sequence, k, &expected_fragments_vec, previous_frags, min_identity, verbose).clone();
         part_frags_map.insert(0,(std::str::from_utf8(sequence).unwrap().to_string(), frag_positions.clone()));
-        //TODO: How to I only split the part sequences and not the overall read sequence anymore?
-        while !frag_positions.is_empty()  && it_ct < 3 {
-            it_ct += 1;
+        //we iterate until we do not have any more frag_positions to investigate
+        while !frag_positions.is_empty()  {
             let mut frag_pos_to_delete= vec![];
             //iterate over the separator positions to find new separators First iteration: All coordinates are the true coordinates on the read, TODO: get correct coords for any further iterations
             for frag_pos in frag_positions.clone().into_iter() {
@@ -268,31 +285,47 @@ fn main() {
                 let frag_len = frag_pos.4.to_owned();
                 let range_start = frag_pos.0 as usize;
                 let edit_dist = frag_pos.2;
-                println!("frag_pos_0: {}, frag_pos_3: {}", endpos, frag_len);
+
+
                 range_end = endpos + 1;
-                println!("Frag start: {}, frag end {} , edit distance {}, frag {}", range_start, range_end, edit_dist, frag_name);
+
                 let sub_range_seq = &sequence[prev_end..range_start];
                 let sub_range_len = sub_range_seq.len();
-                println!("Range starts at {}, length {}", prev_end, sub_range_len);
-                println!("before: {:?}", covering_vec);
+                if verbose{
+                    println!("Frag start: {}, frag end {} , edit distance {}, frag {}", range_start, range_end, edit_dist, frag_name);
+                }
+
                 covering_vec.push((frag_name, range_start, range_end, edit_dist));
-                println!("after: {:?}", covering_vec);
+                if verbose{
+
+                    println!("frag_pos_0: {}, frag_pos_3: {}", endpos, frag_len);
+                    println!("Range starts at {}, length {}", prev_end, sub_range_len);
+                    println!("before: {:?}", covering_vec);
+                    println!("after: {:?}", covering_vec);
+                }
                 frag_pos_to_delete.push(frag_pos);
                 aligned_seg = std::str::from_utf8(&sequence[range_start..range_end]).unwrap();
                 let tmp_inter = Interval { start: range_start, end: range_end };
                 seq_cover_vec.push(tmp_inter);
 
             }
-            println!("fragments to delete: {:?}",frag_pos_to_delete);
+            if verbose{
+                println!("fragments to delete: {:?}",frag_pos_to_delete);
+            }
             for frag_to_delete in frag_pos_to_delete.into_iter(){
                 frag_positions.retain(|x| *x != frag_to_delete);
             }
-            println!("fragments after delete: {:?}",frag_positions);
+
             let mut int_start = 0 as usize;
             seq_cover_vec.sort_by_key(|interval| interval.start);
-            println!("{:?}",seq_cover_vec);
+
+            if verbose{
+
+                println!("{:?}",seq_cover_vec);
+                println!("fragments after delete: {:?}",frag_positions);
+            }
             let mut interlen;
-            let mut add_ranges=vec![];
+            let mut add_ranges= vec![];
             for part in &seq_cover_vec {
                 if part.start > int_start{
                     interlen = part.start - int_start;
@@ -300,10 +333,16 @@ fn main() {
                 else {
                     interlen = 0;
                 }
-                println!("Range from {} to {}", int_start, part.start);
+                if verbose{
+                    println!("Range from {} to {}", int_start, part.start);
+                }
+
                 if interlen > 5 {
-                    this_pos = collect_sep_positions(&sequence[int_start..part.start], k, &expected_fragments_vec, int_start, min_identity);
-                    println!("Added this pos: {:?}", this_pos);
+                    this_pos = collect_sep_positions(&sequence[int_start..part.start], k, &expected_fragments_vec, int_start, min_identity, verbose);
+                    if verbose{
+                        println!("Added this pos: {:?}", this_pos);
+                    }
+
                     if this_pos.is_empty(){
                         add_ranges.push( Interval {start: int_start,end: part.start})
                     }
@@ -315,22 +354,32 @@ fn main() {
                 int_start = part.end;
             }
             seq_cover_vec.append(&mut add_ranges);
-            println!("int_start after for loop {}", int_start);
-            println!("Range from {} to {}", int_start, sequence.len());
+            if verbose{
+                println!("int_start after for loop {}", int_start);
+                println!("Range from {} to {}", int_start, sequence.len());
+            }
+
+
             if sequence.len() - int_start > 5{
-                this_pos = collect_sep_positions(&sequence[int_start..sequence.len()], k, &expected_fragments_vec, int_start, min_identity);
+                this_pos = collect_sep_positions(&sequence[int_start..sequence.len()], k, &expected_fragments_vec, int_start, min_identity, verbose);
                 if this_pos.is_empty(){
                     seq_cover_vec.push( Interval {start: int_start,end: sequence.len()})
                 }
                 else {
                     frag_positions.append(&mut this_pos);
                 }
-                println!("Added this pos: {:?}", this_pos);
+                if verbose{
+                    println!("Added this pos: {:?}", this_pos);
+                }
+
                 frag_positions.append(&mut this_pos);
             }
         }
         covering_vec.sort_by_key(|interval| interval.1);
-        println!("ReadID {}",header_new);
+        if verbose{
+            println!("ReadID {}",header_new);
+
+        }
         println!("covering: {:?}", covering_vec);
         outfile_hashmap.insert(header_new.to_string(),covering_vec);
     }
